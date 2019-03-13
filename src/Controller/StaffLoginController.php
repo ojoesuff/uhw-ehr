9cc223ba37248a4609b2e0e27519c9685d151095
Eoin Hearne <ojoesuff@gmail.com>
2019-03-13 16:16:10 +0000

    added LoginType for from. Need to fix redirect for LoginController

--- src/Controller/StaffLoginController.php
+++ src/Controller/StaffLoginController.php
@@ -2,15 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\UserStaff;
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route; //used to create custom url routes
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class StaffLoginController extends AbstractController {
     /**
-     * @Route("/staff/login", name="staff-login")
+     * @Route("/staff/login", name="staff-login") methods={"GET", "POST"}
      */
+    public function newAction(Request $request) {
 
-     public function staffLogin() {
-         return $this->render('staff-login.html.twig');
-     }
+        $login = new UserStaff();
+        
+        $form = $this->createForm(LoginType::class, $login);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $username = $form['username']->getData();
+            $password = $form['staffPassword']->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $repo = $this->getDoctrine()->getRepository(UserStaff::class)->findOneBy([
+                'username' => $username,
+                'staffPassword' => $password,
+            ]);  
+            
+            if($repo) {
+                $this->redirectToRoute('dashboard');
+            } else {
+                $this->redirectToRoute('admin-create');
+            }
+            
+    }
+
+    return $this->render('staff-login.html.twig', array(
+        'form' => $form->createView() 
+    ));
+}
 }
