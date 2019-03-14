93cd6dba33d3b8be0efeea8daaf6fac692b08797
Eoin Hearne <ojoesuff@gmail.com>
2019-03-14 12:21:51 +0000

    add form validation on backend sing asserts

--- src/Entity/UserStaff.php
+++ src/Entity/UserStaff.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserStaffRepository")
@@ -18,11 +19,13 @@ class UserStaff
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message = "Enter a username")
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message = "Enter a password")
      */
     private $staffPassword;
 
--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -5,7 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 
 class LoginType extends AbstractType
@@ -14,7 +14,7 @@ class LoginType extends AbstractType
 
         $builder
         ->add('username', TextType::class)
-        ->add('staffPassword', TextType::class);
+        ->add('staffPassword', PasswordType::class);
     }
 
 }
