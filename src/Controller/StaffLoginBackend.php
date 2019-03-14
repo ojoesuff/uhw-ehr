b67eefcfb84e22f113b4a7dcac8bfa76981103d8
Eoin Hearne <ojoesuff@gmail.com>
2019-03-14 21:05:15 +0000

    added session, need to add form validation next

---
+++ src/Controller/StaffLoginBackend.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\UserStaff;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response; 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class StaffLoginBackend extends AbstractController {
+    /**
+     * @Route("/backend/staff/login", name="backend-staff-login") methods={"GET", "POST"}
+     */   
+     public function index() {
+
+        $request = Request::createFromGlobals();
+
+        $username = $request->request->get('username', 'none');
+        $password = $request->request->get('password', 'none');
+        $repo = $this->getDoctrine()->getRepository(UserStaff::class)->findOneBy([
+            'username' => $username,
+            'staffPassword' => $password,
+        ]);
+
+        if($repo) {
+
+            $session = new Session();
+            $session->start();
+            
+             //save data to session for authenication
+            $id = $repo->getId();
+            $staffType = $repo->getStaffType();
+            $session->set('id', $id);
+            $session->set('loggedIn', true);
+            $session->set('staffType', $staffType);
+
+            return new Response(
+                "true"
+            );
+        } else {
+            return new Response(
+                "false"
+            );
+        }
+
+     }
+}
--- src/Controller/StaffLoginController.php
+++ src/Controller/StaffLoginController.php
@@ -15,56 +15,58 @@ class StaffLoginController extends AbstractController {
     /**
      * @Route("/staff/login", name="staff-login") methods={"GET", "POST"}
      */
-    public function newAction(Request $request) {
 
-        $login = new UserStaff();
-        
-        $form = $this->createForm(LoginType::class, $login);
-
-        $form->handleRequest($request);
-
-        $loggedIn = false;
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $username = $form['username']->getData();
-            $password = $form['staffPassword']->getData();
-
-            $entityManager = $this->getDoctrine()->getManager();
-
-            $repo = $this->getDoctrine()->getRepository(UserStaff::class)->findOneBy([
-                'username' => $username,
-                'staffPassword' => $password,
-            ]);  
-            
-            if($repo) {
-                //store authenication in session to loads different views
-                $id = $repo->getId();
-                $staffType = $repo->getStaffType();
-                $session = $request->getSession();
-                $session->set('id', $id);
-                $session->set('loggedIn', true);
-                $session->set('staffType', $staffType);
-
-                $loggedIn = true;
-
-
-            } else {
-                //send error message if username and password is not found
-                return $this->render('staff-login.html.twig', array(
-                    'form' => $form->createView(),
-                    'error' => 'Username or password incorrect'
-                ));                
-
-            }
-
-            
-            
+    public function index() {
+        return $this->render('staff-login.html.twig');
     }
 
-    return $this->render('staff-login.html.twig', array(
-        'form' => $form->createView(),
-        'loggedIn' => $loggedIn
-    ));
-}
+//     public function newAction(Request $request) {
+
+//         $login = new UserStaff();
+        
+//         $form = $this->createForm(LoginType::class, $login);
+
+//         $form->handleRequest($request);
+
+//         $loggedIn = false;
+
+//         if ($form->isSubmitted() && $form->isValid()) {
+
+//             $username = $form['username']->getData();
+//             $password = $form['staffPassword']->getData();
+
+//             $entityManager = $this->getDoctrine()->getManager();
+
+//             $repo = $this->getDoctrine()->getRepository(UserStaff::class)->findOneBy([
+//                 'username' => $username,
+//                 'staffPassword' => $password,
+//             ]);  
+            
+//             if($repo) {
+//                 //store authenication in session to loads different views
+//                 $id = $repo->getId();
+//                 $staffType = $repo->getStaffType();
+//                 $session = $request->getSession();
+//                 $session->set('id', $id);
+//                 $session->set('loggedIn', true);
+//                 $session->set('staffType', $staffType);
+
+//                 $loggedIn = true;
+
+
+//             } else {
+//                 //send error message if username and password is not found
+//                 return $this->render('staff-login.html.twig', array(
+//                     'form' => $form->createView(),
+//                     'error' => 'Username or password incorrect'
+//                 ));                
+
+//             }      
+//     }
+
+//     return $this->render('staff-login.html.twig', array(
+//         'form' => $form->createView(),
+//         'loggedIn' => $loggedIn
+//     ));
+// }
 }
