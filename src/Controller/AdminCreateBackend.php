0dca7ae8a9a4d793160e12405847649721411732
Eoin Hearne <ojoesuff@gmail.com>
2019-03-27 16:30:18 +0000

    Added some backend to patient create. Need to fix PDO errors

--- src/Controller/AdminCreateBackend.php
+++ src/Controller/AdminCreateBackend.php
@@ -22,9 +22,9 @@ class AdminCreateBackend extends AbstractController {
          $type = $request->request->get('type');
 
          if($type === "user") {
-            $username = $request->request->get('username', 'none');
-            $password = $request->request->get('password', 'none');
-            $staffType = $request->request->get('staffType', 'none');
+            $username = $request->request->get('username');
+            $password = $request->request->get('password');
+            $staffType = $request->request->get('staffType');
 
             $entityManager = $this->getDoctrine()->getManager();
 
@@ -35,6 +35,48 @@ class AdminCreateBackend extends AbstractController {
 
             $entityManager->persist($userStaff);
             $entityManager->flush(); 
+         }
+
+         if($type === "patient") {
+
+            //get data from request
+            $firstName = $request->request->get('firstName');
+            $middleName = $request->request->get('middleName');
+            $lastName = $request->request->get('lastName');
+            $email = $request->request->get('email');
+            $address = $request->request->get('address');
+            $county = $request->request->get('county');
+            $eircode = $request->request->get('eircode');
+            $country = $request->request->get('country');
+            $dateOfBirth = $request->request->get('dateOfBirth');
+            $telNo = $request->request->get('telNo');
+            if($telNo === "") {
+               $telNo = null;
+            }
+            $mobileNo = $request->request->get('mobileNo');
+            if($mobileNo === "") {
+               $mobileNo = null;
+            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $patient = new Patient();
+            $patient->setFirstName($firstName);
+            $patient->setMiddleNames($middleName);
+            $patient->setLastName($lastName);
+            $patient->setEmail($email);
+            $patient->setAddress($address);
+            $patient->setCounty($county);
+            $patient->setEircode($eircode);
+            $patient->setCountry($country);
+            // $patient->setDateOfBirth($dateOfBirth);
+            $patient->setTelNo($telNo);
+            $patient->setMobileNo($mobileNo);
+
+            $entityManager->persist($patient);
+            $entityManager->flush(); 
+
+            return new Response("New Patient Created!");
          }
 
         
