6dcbb6770061c7136d7776d231a48d6b5bfe5375
Eoin Hearne <ojoesuff@gmail.com>
2019-04-24 21:58:43 +0100

    Started phase 5 admin, created user form finished

--- src/Controller/AdminCreateBackend.php
+++ src/Controller/AdminCreateBackend.php
@@ -19,82 +19,99 @@ class AdminCreateBackend extends AbstractController {
 
          $request = Request::createFromGlobals();
 
+         $entityManager = $this->getDoctrine()->getManager();
+
          $type = $request->request->get('type');
 
-         if($type === "user") {
-            $username = $request->request->get('username');
-            $password = $request->request->get('password');
-            $staffType = $request->request->get('staffType');
+         switch($type) {
+            case "staff":
+               $firstName = $request->request->get('firstName');
+               $lastName = $request->request->get('lastName');
+               if($lastName === null) {
+                  $lastName = "";
+               }
+               $username = $request->request->get('username');
+               $password = $request->request->get('password');
+               $staffType = $request->request->get('staffType');
 
-            $entityManager = $this->getDoctrine()->getManager();
+               //get user with given username
+               $existingUser = $entityManager->getRepository(UserStaff::class)->findOneBy([
+                  "username" => $username
+               ]);
 
-            $userStaff = new UserStaff();
-            $userStaff->setUsername($username);
-            $userStaff->setStaffPassword($password);
-            $userStaff->setStaffType($staffType);
-
-            $entityManager->persist($userStaff);
-            $entityManager->flush(); 
-         }
-
-         if($type === "patient") {
+               //if username already exists, send error
+               if($existingUser) {
+                  return new Response("error");
+               } else {
+                  $userStaff = new UserStaff();
+                  $userStaff->setFirstName($firstName);
+                  $userStaff->setLastName($lastName);
+                  $userStaff->setUsername($username);
+                  $userStaff->setStaffPassword($password);
+                  $userStaff->setStaffType($staffType);
+   
+                  $entityManager->persist($userStaff);
+                  $entityManager->flush();
+                  
+                  return new Response("success");
+               }               
+               break;
             
-            //get data from request
-            $firstName = $request->request->get('firstName');
-            $middleName = $request->request->get('middleName');
-            $lastName = $request->request->get('lastName');
-            $email = $request->request->get('email');
-            $address = $request->request->get('address');
-            $county = $request->request->get('county');
-            $eircode = $request->request->get('eircode');
-            $country = $request->request->get('country');
-            //convert to DateTime object
-            $dateOfBirth = date_create($request->request->get('dateOfBirth'));
-            //current time
-            $dateCreated = date_create();
-            $telNo = $request->request->get('telNo');
-            //prevent empty string being passed to DB for number
-            if($telNo === "") {
-               $telNo = null;
-            }
-            $mobileNo = $request->request->get('mobileNo');
-            if($mobileNo === "") {
-               $mobileNo = null;
-            }            
+            case "patient":
+               //get data from request
+               $firstName = $request->request->get('firstName');
+               $middleName = $request->request->get('middleName');
+               $lastName = $request->request->get('lastName');
+               $email = $request->request->get('email');
+               $address = $request->request->get('address');
+               $county = $request->request->get('county');
+               $eircode = $request->request->get('eircode');
+               $country = $request->request->get('country');
+               //convert to DateTime object
+               $dateOfBirth = date_create($request->request->get('dateOfBirth'));
+               //current time
+               $dateCreated = date_create();
+               $telNo = $request->request->get('telNo');
+               //prevent empty string being passed to DB for number
+               if($telNo === "") {
+                  $telNo = null;
+               }
+               $mobileNo = $request->request->get('mobileNo');
+               if($mobileNo === "") {
+                  $mobileNo = null;
+               }   
 
-            $entityManager = $this->getDoctrine()->getManager();
+               $patient = new Patient();
+               $patient->setFirstName($firstName);
+               $patient->setMiddleNames($middleName);
+               $patient->setLastName($lastName);
+               $patient->setEmail($email);
+               $patient->setAddress($address);
+               $patient->setCounty($county);
+               $patient->setEircode($eircode);
+               $patient->setCountry($country);
+               $patient->setDateOfBirth($dateOfBirth);
+               $patient->setTelNo($telNo);
+               $patient->setMobileNo($mobileNo);
+               $patient->setDateCreated($dateCreated);            
 
-            $patient = new Patient();
-            $patient->setFirstName($firstName);
-            $patient->setMiddleNames($middleName);
-            $patient->setLastName($lastName);
-            $patient->setEmail($email);
-            $patient->setAddress($address);
-            $patient->setCounty($county);
-            $patient->setEircode($eircode);
-            $patient->setCountry($country);
-            $patient->setDateOfBirth($dateOfBirth);
-            $patient->setTelNo($telNo);
-            $patient->setMobileNo($mobileNo);
-            $patient->setDateCreated($dateCreated);            
+               //default value in entity being written as null when input is empty
+               //below solution found at https://stackoverflow.com/questions/3376881/default-value-in-doctrine
+               if ($patient->getStatus() === null) {
+                  $patient->setStatus('Pending');
+               }
+               if ($patient->getPriority() === null) {
+                  $patient->setPriority('Low');
+               }
 
-            //default value in entity being written as null when input is empty
-            //below solution found at https://stackoverflow.com/questions/3376881/default-value-in-doctrine
-            if ($patient->getStatus() === null) {
-               $patient->setStatus('Pending');
-            }
-            if ($patient->getPriority() === null) {
-               $patient->setPriority('Low');
-            }
+               $entityManager->persist($patient);
+               $entityManager->flush(); 
 
-            $entityManager->persist($patient);
-            $entityManager->flush(); 
+               return new Response("success");
+               break;
 
-            return new Response("New Patient Created!");
-         }
+         } //end switch
 
-        
-
-        return new Response("Success");
-     }
+        return new Response("none");
+     } //end createStaff
 }
